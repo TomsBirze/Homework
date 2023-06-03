d408943bac2113a2abe94b25a86d0e4223b5c69b
TomsBirze <toms.birze@inbox.lv>
2023-06-03 10:01:28 +0300

    HomeworkTwo second commit

--- src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
+++ src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
@@ -10,50 +10,15 @@ public class PhoneKeyPad {
         String input = in.next().toLowerCase();
 //        ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), WXYZ(9).
         switch (input) {
-            case "a":
-            case "b":
-            case "c":
-                System.out.print(2);
-                break;
-            case "d":
-            case "e":
-            case "f":
-                System.out.print(3);
-                break;
-            case "g":
-            case "h":
-            case "i":
-                System.out.print(4);
-                break;
-            case "j":
-            case "k":
-            case "l":
-                System.out.print(5);
-                break;
-            case "m":
-            case "n":
-            case "o":
-                System.out.print(6);
-                break;
-            case "p":
-            case "q":
-            case "r":
-            case "s":
-                System.out.print(7);
-                break;
-            case "t":
-            case "u":
-            case "v":
-                System.out.print(8);
-                break;
-            case "w":
-            case "x":
-            case "y":
-            case "z":
-                System.out.print(9);
-                break;
-            default:
-                System.out.println("Wrong input!");
+            case "a", "b", "c" -> System.out.print(2);
+            case "d", "e", "f" -> System.out.print(3);
+            case "g", "h", "i" -> System.out.print(4);
+            case "j", "k", "l" -> System.out.print(5);
+            case "m", "n", "o" -> System.out.print(6);
+            case "p", "q", "r", "s" -> System.out.print(7);
+            case "t", "u", "v" -> System.out.print(8);
+            case "w", "x", "y", "z" -> System.out.print(9);
+            default -> System.out.println("Wrong input!");
         }
     }
 }
